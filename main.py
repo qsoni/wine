45b7c48afa91b61647f068b190a2c59c1535b08b
qu1z5 <84225471+qsoni@users.noreply.github.com>
2022-03-16 19:17:24 +0300

    исправил код

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
 import collections
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
+import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
-
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
+    load_dotenv()
+    production_file_path = os.getenv('PATH_TO_FILE')
+    drinks = pandas.read_excel(production_file_path, sheet_name='Лист1', na_values= None, keep_default_na=False).to_dict(orient='records')
 
-    drinks = pandas.read_excel('wine.xlsx', sheet_name='Лист1', na_values= None, keep_default_na=False).to_dict(orient='records')
-
-    now = datetime.datetime.now()
-    year_of_foundation = 1920
-    year = now.year
-
+    current_year = datetime.datetime.now().year
+    foundation_year = 1920
     structured_drinks = collections.defaultdict(list)
 
     for wine in drinks:
         structured_drinks[wine ['Категория']].append(wine)
 
-
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-
     template = env.get_template('template.html')
     rendered_page = template.render(
-        years=year-year_of_foundation,
+        years_since_the_foundation=current_year-foundation_year,
         drinks=structured_drinks
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
-
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
